abc4a65090bfc8a4038f3b324b67d280f4aac8d5
Arman <armandwi.pangestu7@gmail.com>
2023-12-21 02:56:15 +0700

    feat: 3 - Struktur Folder, Routes dan View

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Arman Dwi Pangestu",
+        "email" => "armandwi.pangestu7@gmail.com",
+        "image" => "arman.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
