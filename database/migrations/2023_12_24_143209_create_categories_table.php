12946d70aa0681deb4e41663ce2023be75c13156
Arman <armandwi.pangestu7@gmail.com>
2023-12-25 04:53:51 +0700

    feat: 8 - Post Category dan Eloquent Relationship

--- database/migrations/2023_12_24_143209_create_categories_table.php
+++ database/migrations/2023_12_24_143209_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,10 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id');
-            $table->string('title');
+            $table->string('name')->unique();
             $table->string('slug')->unique();
-            $table->string('author');
-            $table->text('excerpt');
-            $table->text('body');
-            $table->timestamp('publish_at')->nullable();
             $table->timestamps();
         });
     }
@@ -33,6 +28,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('categories');
     }
 }
