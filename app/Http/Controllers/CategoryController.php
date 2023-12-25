824916382eda6be0d2b04497d3f0b3986e2383ae
Arman <armandwi.pangestu7@gmail.com>
2023-12-25 17:49:25 +0700

    feat: Category Controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        return view('categories', [
+            'title' => 'Post Categories',
+            'categories' => Category::all()
+        ]);
+    }
+
+    public function show(Category $category)
+    {
+        return view('category', [
+            'title' => $category->name,
+            'posts' => $category->posts,
+            'category' => $category->name
+        ]);
+    }
+}
